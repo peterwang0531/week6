4ee5250e4165abb611d753a48427827957708730
Jeff Cohen <cohen.jeff@gmail.com>
2019-02-16 15:36:46 -0600

    At the end of class

--- demo/src/app.js
+++ demo/src/app.js
@@ -3,20 +3,50 @@ import ReactDOM from 'react-dom';
 import $ from 'jquery';
 import './app.css'
 
+
 class App extends Component {
 
+  state = { movies: [] }
+
+  handleClick = (event) => {
+    event.preventDefault();
+    const api_key_param = "api_key=bde024f3eb43f597aafe01ed9c9098c6"
+    const language_param = "language=en-US"
+    const region_param = "region=US"
+    const top_rated_url = "https://api.themoviedb.org/3/movie/top_rated"
+    const full_url = `${top_rated_url}?${api_key_param}&${language_param}&${region_param}`
+    $.getJSON(full_url, this.receiveTopRatedData)
+  }
+
+  receiveTopRatedData = (data) => {
+    console.log(data);
+    // this.state.movies = data.results
+    this.setState({movies: data.results})
+  }
+
   render() {
+
+    let content = this.state.movies.map((movie) => {
+      return (
+        <div className="col-sm-3 text-center" key={movie.id}>
+            <img src={"http://image.tmdb.org/t/p/w185/" + movie.poster_path}
+                className="img-fluid"/>
+            <p>{movie.release_date.substr(0,4)}</p>
+        </div>
+        )
+      })
+
     return (
       <div className="container mt-5">
 
         <div className="row mb-5">
           <div className="col">
-            <a className="btn btn-primary" href="#">Get Top-Rated Movies</a>
+            <a onClick={this.handleClick} className="btn btn-primary" href="#">Get Top-Rated Movies</a>
           </div>
         </div>
 
         <div className="row">
-          // Movie posters will go here
+          {content}
         </div>
 
       </div>
